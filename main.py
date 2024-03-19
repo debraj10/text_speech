f5a6146e8e034f8e68ea6a9930126d03b793f136
debraj10 <117508114+debraj10@users.noreply.github.com>
2024-03-19 11:18:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ Created on Mon Mar 11 19:17:35 2024
 @author: USER
 """
 
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from gtts import gTTS
 import os
@@ -16,27 +17,28 @@ class TextData(BaseModel):
     text: str
     language: str
 
-def text_to_speech(text: str, language: str):
+@app.post("/text_to_speech/")
+async def produce_text_to_speech(text_data: TextData):
+    language = text_data.language
+    text = text_data.text
+
     if language == 'en':
         tts = gTTS(text, lang='en')
         tts.save('output.mp3')
-        os.system('start output.mp3')
     elif language == 'hi':
         tts = gTTS(text, lang='hi')
         tts.save('output.mp3')
-        os.system('start output.mp3')
     elif language == 'bn':
         tts = gTTS(text, lang='bn')
         tts.save('output.mp3')
-        os.system('start output.mp3')
     else:
         print("Unsupported language")
 
-@app.post("/text_to_speech/")
-async def produce_text_to_speech(text_data: TextData, background_tasks: BackgroundTasks):
-    background_tasks.add_task(text_to_speech, text_data.text, text_data.language)
-    return {"message": "Text to speech conversion has been started."}
+    filepath = './output.mp3'
+    filename = os.path.basename(filepath)
+    headers = {'Content-Disposition': f'attachment; filename="{filename}"'}
+    return FileResponse(filepath, headers=headers, media_type="audio/mp3")
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
